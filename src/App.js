04de3b0480c2dfe380720f1eeff206cb118d1e4e
ymk-coder-star <ymkrausz68@gmail.com>
2026-05-31 10:25:13 +0100

    main commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
+
+//styles
 import './App.css';
 
+//pages and components
+import Dashboard from './pages/dashboard/Dashboard';
+import Create from './pages/create/Create';
+import Signup from './pages/signup/Signup';
+import Login from './pages/login/Login';
+import Project from './pages/project/Project';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import OnlineUsers from './components/OnlineUsers';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { user, authIsReady } = useAuthContext();
+
+	return (
+		<div className="App">
+			{authIsReady && (
+				<BrowserRouter>
+					{user && <Sidebar />}
+					<div className="container">
+						<Navbar />
+						<Switch>
+							<Route exact path="/">
+								{!user && <Redirect to="/login" />}
+								{user && <Dashboard />}
+							</Route>
+							<Route path="/create">
+								{!user && <Redirect to="/login" />}
+								{user && <Create />}
+							</Route>
+							<Route path="/projects/:id">
+								{!user && <Redirect to="/login" />}
+								{user && <Project />}
+							</Route>
+							<Route path="/login">
+								{user && <Redirect to="/" />}
+								{!user && <Login />}
+							</Route>
+							<Route path="/signup">
+								{user && <Redirect to="/" />}
+								{!user && <Signup />}
+							</Route>
+						</Switch>
+					</div>
+					{user && <OnlineUsers />}
+				</BrowserRouter>
+			)}
+		</div>
+	);
 }
 
 export default App;
